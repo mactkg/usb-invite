f2c46ef41b8f2c3324925346dd0ea82654ad6222
mactkg <mactkg@gmail.com>
2016-06-12 13:12:39 +0900

    cron

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var Slack = require('slack-node');
+var CronJob = require('cron').CronJob;
 apiToken = require('./key.json').token;
-usb = "U09G6N0GH"
+target = "U09G6N0GH"
 random = "C03RYFN2M"
 slack = new Slack(apiToken);
 
-slack.api("channels.info", { channel: random }, function(err, res) {
-  var i = res.channel.members.indexOf(usb)
-  if (i < 0) {
-    console.log('invite')
-    slack.api("channels.invite", { channel: rnadom, user: usb })
-  }
-});
+new CronJob('*/5 * * * * *', function() {
+  slack.api("channels.info", { channel: random }, function(err, res) {
+    var i = res.channel.members.indexOf(target)
+    if (i < 0) {
+      console.log('invite')
+      slack.api("channels.invite", { channel: random, user: target })
+    }
+  });
+}, null, true, 'Asia/Tokyo')
